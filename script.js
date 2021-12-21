15bf8f6c5a0e5637a14d1ce01a28fd50152ba0b7
Nadezhda Kravtsova <skykerch@gmail.com>
2021-12-21 22:34:40 +0300

    practick 2 condition&cycle

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict'
 let numberOfFilms = +prompt('сколько фильмов вы уже посмотрели', '');
 let personalMovieDB = {
     count: numberOfFilms,
@@ -7,11 +8,45 @@ let personalMovieDB = {
     privat: false
 }
 
-let a = prompt('Один из последних просмотренных фильмов', ''),
-    b = prompt('На сколько вы его оцените', ''),
-    c = prompt('Один из последних просмотренных фильмов', ''),
-    d = prompt('На сколько вы его оцените', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-console.log(personalMovieDB)
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из последних просмотренных фильмов', ''),
+        b = prompt('На сколько вы его оцените', '');
+
+    if (a != null && a.length < 50 & a != '' && b != null && b.length < 50 && b != '') {
+        personalMovieDB.movies[a] = b;
+        console.log('done')
+    } else {
+        console.log('error');
+        i--;
+    }
+
+
+}
+// switch (numberOfFilms) {
+//     case (numberOfFilms < 10):
+//         alert('Просмотрено довольно мало фильмов')
+//         break;
+//     case (numberOfFilms >= 10 && numberOfFilms <= 30):
+//         alert('Вы классический зритель')
+//         break;
+//     case (numberOfFilms > 30):
+//         alert('Вы киноман')
+//         break;
+
+//     default:
+//         alert('произошла ошибка')
+
+//         break;
+// }
+
+if (numberOfFilms < 10) {
+    alert('Просмотрено довольно мало фильмов')
+} else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+    alert('Вы классический зритель')
+} else if (numberOfFilms > 30) {
+    alert('Вы киноман')
+} else {
+    alert('произошла ошибка')
+}
