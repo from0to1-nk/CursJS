aeb8639b8cf2d53efc4c1d0b0ba919fa9d8d63a0
Nadezhda Kravtsova <skykerch@gmail.com>
2021-12-21 20:46:58 +0300

    first practic

---
+++ script.js
@@ -0,0 +1,17 @@
+let numberOfFilms = +prompt('сколько фильмов вы уже посмотрели', '');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    privat: false
+}
+
+let a = prompt('Один из последних просмотренных фильмов', ''),
+    b = prompt('На сколько вы его оцените', ''),
+    c = prompt('Один из последних просмотренных фильмов', ''),
+    d = prompt('На сколько вы его оцените', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB)
